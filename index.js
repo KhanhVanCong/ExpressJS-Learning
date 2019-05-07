215647d4b78eb7ab930075df769f350d7aa87d06
KhanhVanCong <khanhvc2310@gmail.com>
2019-05-07 22:09:33 +0700

    Express Router

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
-var db = low(adapter);
-var shortid = require('shortid');
+var userRoutes = require('./routes/user.route'); 
+
 var port = 3000;
+var app = express();
 
 
-db.defaults({ users: [] })
-  .write()
+
 
 
 app.set('view engine', 'pug');
@@ -26,42 +22,8 @@ app.get('/', (request, response) => {
         name: "Hello Coders.Tokyo"
     });
 });
-app.get('/users', (req, res) => {
-    console.log(db.get('users').value());
-    res.render('users/index', {
-        'users': db.get('users').value()
-    })
-});
 
-app.get('/users/search', (req, res) => {
-    var q = req.query.q;
-    var matchedUsers = db.get('users').value().filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
-    res.render('users/index', {
-        'users': matchedUsers,
-        'keyword': q
-    })
-})
+app.use('/users', userRoutes)
 
-app.get('/users/create', (req, res)=> {
-    res.render('users/create')
-})
-
-app.get('/users/:id', (req, res) => {
-    var id = req.params.id;
-    if(!isNaN(req.params.id))
-    {
-        id = parseInt(req.params.id);
-    } 
-    var user = db.get('users').find({id: id}).value();
-    res.render('users/view', {
-        user: user
-    });
-})
-
-app.post('/users/create', (req, res) => {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users')
-})
 
 app.listen(port, () => console.log('Server listening on port 3000'));
---
+++ routes/user.route.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+var shortid = require('shortid');
+
+
+
+router.get('/', (req, res) => {
+    console.log(db.get('users').value());
+    res.render('users/index', {
+        'users': db.get('users').value()
+    })
+});
+
+router.get('/search', (req, res) => {
+    var q = req.query.q;
+    var matchedUsers = db.get('users').value().filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
+    res.render('users/index', {
+        'users': matchedUsers,
+        'keyword': q
+    })
+})
+
+router.get('/create', (req, res)=> {
+    res.render('users/create')
+})
+
+router.get('/:id', (req, res) => {
+    var id = req.params.id;
+    if(!isNaN(req.params.id))
+    {
+        id = parseInt(req.params.id);
+    } 
+    var user = db.get('users').find({id: id}).value();
+    res.render('users/view', {
+        user: user
+    });
+})
+
+router.post('/create', (req, res) => {
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users')
+})
+module.exports = router;
